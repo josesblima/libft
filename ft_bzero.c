83cf240df2fb29031477cec1b13e8286b835049a
josesblima <josesblima@gmail.com>
2024-04-09 16:18:55 +0100

    up to bzero inc

---
+++ ft_bzero.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void   *ft_bzero(void *buffer, size_t len)
+{
+    int i;
+    unsigned char *buff;
+
+    i = 0;
+    buff = (unsigned char *)buffer;
+    while (i < len)
+    {
+        buff[i] = 0;
+        i++;
+    }
+    return (buffer);
+}
+/*
+int main(void)
+{
+    char  str[11];
+    char  *str2;
+
+    strcpy(str, "1234567890");
+    str2 = str + 3;
+    ft_bzero(str2, 3);
+    printf("str = %s\n", str);
+    return (0);
+}*/
---
+++ ft_memset.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void   *ft_memset(void *buffer, int c, size_t len)
+{
+    int i;
+    unsigned char *buff;
+
+    i = 0;
+    buff = (unsigned char *)buffer;
+    while (i < len)
+    {
+        buff[i] = c;
+        i++;
+    }
+    return (buffer);
+}
+/*
+int main(void)
+{
+    char  str[11];
+
+    strcpy(str, "1234567890");
+    ft_memset(str, 48, 3);
+    printf("%s\n", str);
+    return (0);
+}*/
