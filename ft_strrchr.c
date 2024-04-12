928b9e455d46e2b5ddc35896724d7a350c24d496
josesblima <josesblima@gmail.com>
2024-04-13 00:30:46 +0100

    strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -7,10 +7,15 @@ char *ft_strchr(const char *s, int c)
 
     i = 0;
     while (s[i])
+        i++;
+    if (i == 0)
+        return ((char *) s);
+    i--;
+    while (s[i])
     {
         if (s[i] == c)
             return ((char *) (s + i));
-        i++;
+        i--;
     }
     if ((char *)(s + i) == 0)
         return ((char *)(s + i));
@@ -20,8 +25,8 @@ char *ft_strchr(const char *s, int c)
 int main(void)
 {
     char  str[11] = "0123456789";
-    printf("%s\n", ft_strchr(str, 'f'));
+    printf("%s\n", ft_strchr(str, '3'));
     char  str2[11] = "0123456789";
-    printf("%s\n", strchr(str2, 'f'));
+    printf("%s\n", strchr(str2, '3'));
     return(0);
 }*/
