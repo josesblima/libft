3ce4edb7eaa4247caaea651baf97f26799c619ea
josesblima <josesblima@gmail.com>
2024-04-14 15:28:25 +0100

    memchr

---
+++ ft_memchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+void  *ft_memchr(const void *s, int c, size_t n)
+{
+    int i;
+    char *str;
+
+    i = 0;
+    str = (char *)s;
+    while (i < n)
+    {
+        if (*(str + i) == c)
+            return (str + i);
+        i++;
+    }
+    return (NULL);
+}
+
+/* int main(void) */
+/* { */
+/*     printf("%s\n", (char *)ft_memchr("0123456789", 50, 0)); */
+/*     printf("%s\n", (char *)memchr("0123456789", 50, 0)); */
+/*     return (0); */
+/* } */
---
+++ ft_strncmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    int i;
+
+    i = 0;
+    if (n == 0)
+        return (0);
+    while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+        i++;
+    if (i == n)
+        i--;
+    return (s1[i] - s2[i]);
+}
+
+/* int main(void) */
+/* { */
+/*     printf("ft_strncmp() = %d\n", ft_strncmp("qwertyu", "qwerytu", 0)); */
+/*     return (0); */
+/* } */
