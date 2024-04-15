61e95032a65246f765b3ed1c08b46b4193c2ebbc
josesblima <josesblima@gmail.com>
2024-04-15 15:15:10 +0100

    added strdup, all libc done

---
+++ ft_strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char  *ft_strdup(const char *s)
+{
+    int i;
+    char *res;
+
+    i = 0;
+    while (s[i])
+        i++;
+    res = (char *)malloc((i + 1) * sizeof(char));
+    i= 0;
+    while (s[i])
+    {
+        res[i] = s[i];
+        i++;
+    }
+    res[i] = 0;
+    return (res);
+}
+
+/* int main(void) */
+/* { */
+/*     printf("%s\n", ft_strdup("copy this")); */
+/*     return (0); */
+/* } */
