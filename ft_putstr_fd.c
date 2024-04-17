6a6acd40473f573e2013aedd0aade45d0cce131a
josesblima <josesblima@gmail.com>
2024-04-17 04:09:04 +0100

    putstr

---
+++ ft_putstr_fd.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void  ft_putstr_fd(char *s, int fd)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+    {
+        write(fd, &s[i], 1);
+        i++;
+    }
+}
+
+/* int main(void) */
+/* { */
+/*     ft_putstr_fd("This is cool!", 2); */
+/*     return (0); */
+/* } */
+//cc ft_putstr_fd.c -o putstr && ./putstr >stdout.txt 2>stderr.txt
