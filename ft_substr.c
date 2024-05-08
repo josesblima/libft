581c1f4e3757720fef7ee9159e8805ca4d5d4b11
josesblima <josesblima@gmail.com>
2024-05-08 12:47:12 +0100

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,60 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int check_set(char c, char const *set)
+char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    int i;
-
-    i = 0;
-    while (set[i])
-    {
-        if (set[i] == c)
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
-static int ft_strlen(const char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
-}
-char  *ft_strtrim(char const *s1, char const *set)
-{
-    int right;
-    int left;
     char *res;
-    int i;
+    unsigned int i;
 
-    right = ft_strlen(s1) - 1;
-    left = 0;
-    while (s1[left] && check_set(s1[left], set))
-        left++;
-    printf("left = %d\n", left);
-    while (s1[right] && check_set(s1[right], set))
-        right--;
-    printf("right = %d\n", right);
-    res = (char *)malloc((right - left + 1) * sizeof(char));
+    i = start;
+    while (s[i])
+        i++;
+    res = (char *)malloc((len + 1) * sizeof(char));
     if (!res)
         return (NULL);
     i = 0;
-    while (left < right)
+    while (s[start] && i < len)
     {
-        res[i] = s1[left];
-        left++;
+        res[i] = s[start];
+        start++;
         i++;
     }
     res[i] = 0;
     return (res);
-}
-
-int main(void)
-{
-    printf("%s\n", ft_strtrim("qwer1234567890rewqe", "qwer"));
-    return (0);
 }
