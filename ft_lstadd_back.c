2ea1fd750810f062689e5908f7ca8d35ad1dc284
josesblima <josesblima@gmail.com>
2024-05-15 15:32:00 +0100

    lstlast.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+void    ft_lstadd_back(t_list **lst, t_list *new)
+{
+    if (!new)
+        return ;
+    if (!*lst)
+    {
+        *lst = new;
+        return ;
+    }
+    (*lst)->next = new;
+}
